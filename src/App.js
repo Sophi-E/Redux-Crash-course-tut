93ba718326d1aaefeb0364116fcf12119de3735a
Sophia <sophierithous@gmail.com>
2020-02-17 14:44:34 +0100

    redux
    integrated redux

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React, { Component } from "react";
 import "./styles.css";
 import Posts from "./components/Posts";
 import PostForm from "./components/PostForm";
+import { Provider } from "react-redux";
+import store from "./Store";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Reduxs</h1>
-        <PostForm />
-        <Posts />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <h1>Redux</h1>
+          <PostForm />
+          <Posts />
+        </div>
+      </Provider>
     );
   }
 }
